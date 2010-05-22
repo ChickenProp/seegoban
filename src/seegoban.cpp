3715ffd21f7af3f1608d1e93a25b560f7ba2526d
Philip Hazelden <philip.hazelden@gmail.com>
2010-05-22 14:00:04 +0100

    Draw a grid.

--- src/seegoban.cpp
+++ src/seegoban.cpp
@@ -19,9 +19,40 @@ int main(int argc, char **argv) {
 	board_sprite.SetImage(board_image);
 	board_sprite.Resize(640, 480);
 
+	sf::Shape circle = sf::Shape::Circle(0, 0, 5, sf::Color(0, 0, 0, 0), 1, sf::Color(255, 0, 0));
+	sf::Shape line;
+
 	while (window.IsOpened()) {
 		handleEvents(window);
 		window.Draw(board_sprite);
+
+		for (int i = 0; i < current_corner; i++) {
+			circle.SetPosition(corners[i]); 
+			window.Draw(circle);
+		}
+
+		if (current_corner >= 4) {
+			sf::Vector2f u = (corners[1] - corners[0])/(boardsize-1.f);
+			sf::Vector2f v = (corners[2] - corners[3])/(boardsize-1.f);
+			for (int i = 0; i < boardsize; i++) {
+				line = sf::Shape::Line(corners[0] + (float)i*u,
+				                       corners[3] + (float)i*v,
+				                       1, sf::Color(255,0,0));
+
+				window.Draw(line);
+			}
+
+			u = (corners[3] - corners[0])/(boardsize-1.f);
+			v = (corners[2] - corners[1])/(boardsize-1.f);
+			for (int i = 0; i < boardsize; i++) {
+				line = sf::Shape::Line(corners[0] + (float)i*u,
+				                       corners[1] + (float)i*v,
+				                       1, sf::Color(255,0,0));
+
+				window.Draw(line);
+			}
+		}
+
 		window.Display();
 	}
 }
@@ -43,6 +74,16 @@ void handleEvent(sf::RenderWindow &window, sf::Event e) {
 		corners[current_corner] = sf::Vector2f(e.MouseButton.X,
 		                                       e.MouseButton.Y);
 		current_corner++;
+		break;
+	case sf::Event::KeyPressed:
+		switch (e.Key.Code) {
+		case sf::Key::Escape:
+			window.Close();
+			break;
+		case sf::Key::R:
+			current_corner = (current_corner == 0 ? 4 : 0);
+			break;
+		}
 		break;
 	}
 }
